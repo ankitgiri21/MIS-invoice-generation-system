404a09657b0c2a28516433f208735f2d24d1a6a1
Ankit Giri <ankitgiri21@gmail.com>
2025-04-09 14:09:16 +0530

    3 commit

---
+++ Backend/mis-invoice-generation-system/src/main/java/com/codeb/mis_invoice_generation_system/MisInvoiceGenerationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.codeb.mis_invoice_generation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisInvoiceGenerationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisInvoiceGenerationSystemApplication.class, args);
+	}
+
+}
---
+++ Backend/mis-invoice-generation-system/src/test/java/com/codeb/mis_invoice_generation_system/MisInvoiceGenerationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeb.mis_invoice_generation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisInvoiceGenerationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
